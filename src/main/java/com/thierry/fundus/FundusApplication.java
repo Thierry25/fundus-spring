a3510ec4b4eb8f4a3268d64ffdb0f75899019254
Thierry <marcelinthierry@gmail.com>
2023-01-14 06:27:05 -0500

    Add initial files

---
+++ src/main/java/com/thierry/fundus/FundusApplication.java
@@ -0,0 +1,13 @@
+package com.thierry.fundus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thierry/fundus/FundusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thierry.fundus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
